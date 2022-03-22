f2381f3c3d791ffb9da1a19b7fd7b7908d787cea
abdulkhaliq9226 <98957678+abdulkhaliq9226@users.noreply.github.com>
2022-03-22 16:26:15 +0500

    version 2.0

--- src/components/MenuItems.jsx
+++ src/components/MenuItems.jsx
@@ -19,28 +19,28 @@ function MenuItems() {
       defaultSelectedKeys={[pathname]}
     >
       <Menu.Item key="/quickstart">
-        <NavLink to="/quickstart">🚀 Quick Start</NavLink>
+        <NavLink to="/quickstart">Home</NavLink>
       </Menu.Item>
       <Menu.Item key="/wallet">
-        <NavLink to="/wallet">👛 Wallet</NavLink>
+        <NavLink to="/wallet">Wallet</NavLink>
       </Menu.Item>
       <Menu.Item key="/1inch">
-        <NavLink to="/1inch">🏦 Dex</NavLink>
+        <NavLink to="/1inch">Dex</NavLink>
       </Menu.Item>
       <Menu.Item key="onramp">
-        <NavLink to="/onramp">💵 Fiat</NavLink>
+        <NavLink to="/onramp">Fiat</NavLink>
       </Menu.Item>
       <Menu.Item key="/erc20balance">
-        <NavLink to="/erc20balance">💰 Balances</NavLink>
+        <NavLink to="/erc20balance">Balances</NavLink>
       </Menu.Item>
       <Menu.Item key="/erc20transfers">
-        <NavLink to="/erc20transfers">💸 Transfers</NavLink>
+        <NavLink to="/erc20transfers">Transfers</NavLink>
       </Menu.Item>
       <Menu.Item key="/nftBalance">
-        <NavLink to="/nftBalance">🖼 NFTs</NavLink>
+        <NavLink to="/nftBalance">NFTs</NavLink>
       </Menu.Item>
       <Menu.Item key="/contract">
-        <NavLink to="/contract">📄 Contract</NavLink>
+        <NavLink to="/contract">Contract</NavLink>
       </Menu.Item>
     </Menu>
   );
--- src/components/QuickStart.jsx
+++ src/components/QuickStart.jsx
@@ -1,30 +1,44 @@
-// import { Card, Timeline, Typography } from "antd";
-// import React, { useMemo } from "react";
-// import { useMoralis } from "react-moralis";
+import { Card } from "antd";
 
-// const { Text } = Typography;
-
-// const styles = {
-//   title: {
-//     fontSize: "20px",
-//     fontWeight: "700",
-//   },
-//   text: {
-//     fontSize: "16px",
-//   },
-//   card: {
-//     boxShadow: "0 0.5rem 1.2rem rgb(189 197 209 / 20%)",
-//     border: "1px solid #e7eaf3",
-//     borderRadius: "0.5rem",
-//   },
-//   timeline: {
-//     marginBottom: "-45px",
-//   },
-// };
+const styles = {
+  title: {
+    fontSize: "30px",
+    fontWeight: "600",
+  },
+  header: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    gap: "5px",
+  },
+  card: {
+    boxShadow: "0 0.5rem 1.2rem rgb(189 197 209 / 20%)",
+    border: "1px solid #e7eaf3",
+    borderRadius: "1rem",
+    width: "700px",
+    fontSize: "16px",
+    fontWeight: "500",
+  },
+  cardBody: {
+    fontWeight: "normal"
+  }
+};
 
 export default function QuickStart() {
-  return `<div>
-      TEST
-    </div>
-    `;
-}
+  return (
+    <Card
+      style={styles.card}
+      title={
+        <div style={styles.header}>
+          Welcome to my DApp
+        </div>
+      }
+    >
+      <div >
+        <img style={{ display: "block", margin: "0 auto" }} src="https://dewey.tailorbrands.com/production/brand_version_mockup_image/685/7009016685_ed272c55-c923-4238-9047-3aa540c2cd70.png?cb=1647947346" />
+        <br></br>
+      </div>
+      <div style={styles.cardBody}> A DApp which connects with your Wallet, and can be used to transfer funds, hold FIAT, See Balances, List down the Transfers, See NFTs, List contracts and so on.</div>
+    </Card >
+  );
+};
